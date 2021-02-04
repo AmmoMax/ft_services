346abd08052a32d2575352c917c8a7d3f0efb1ae
AmmoMax <steelratmax@gmail.com>
2021-02-04 22:27:55 +0300

    added secret for mysql pods - username and pass

--- src/mysql/startup.sh
+++ src/mysql/startup.sh
@@ -8,7 +8,7 @@ done
 
 echo "**Setup my DB: create DB, create user, give him privileges! **"
 
-echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-echo "CREATE USER 'amayor'@'%' IDENTIFIED BY 'amayor';" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'amayor'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "CREATE DATABASE $DB_NAME;" | mysql -u root --skip-password
+echo "CREATE USER '$USER_NAME'@'%' IDENTIFIED BY '$USER_PASSWORD';" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER_NAME'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
