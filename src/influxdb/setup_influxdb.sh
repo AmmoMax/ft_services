a451b7fb215744723027a5ff31a343175273aee1
AmmoMax <steelratmax@gmail.com>
2021-03-01 23:18:58 +0300

    set influxdb url and token from environment vars

--- src/influxdb/setup_influxdb.sh
+++ src/influxdb/setup_influxdb.sh
@@ -16,13 +16,11 @@ else
 	echo "INFLUX_TOKEN = $INFLUX_TOKEN"
 fi
 
-echo "** Creating InfluxDB config **"
-# influx config create --active -n ft_services -u $INFLUXDB_URL -t $INFLUX_TOKEN -o ft_services
-
 until influx ping
 do
 	echo "** influxd is not ready.. **"
 	sleep 1
 done
 
-influx setup --org ft_services --bucket telegraf --host http://172.17.0.2:8086 --username amayor --password amayor1234 -t "Qq_YHXnShisgcLB6M1pasbc1oeBNHwnKFvqZGr5I1pU3XifMJk8WDBjsfEw7q-pzWdsXkhQE-ABywtaIPw0JSQ==" --force
+echo "** Creating InfluxDB config **"
+influx setup --org ft_services --bucket telegraf --host $INFLUXDB_URL --username amayor --password amayor1234 -t $INFLUX_TOKEN --force
