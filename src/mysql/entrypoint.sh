54e04f0c30d2e2c1c1a5df132c1fe8fd5b166ba9
AmmoMax <steelratmax@gmail.com>
2021-03-03 18:22:49 +0300

    minor fixes for mysql entrypoint script

--- src/mysql/entrypoint.sh
+++ src/mysql/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 echo "** Run setup MySQL server **"
 source startup.sh &
+
 echo "** Fix /etc/my.cnf.d/mariadb-server.cnf for access from outside the container"
 sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+
 echo "** Install service tables in MySQL DB **"
 /usr/bin/mysql_install_db --user=mysql --datadir="/var/lib/mysql"
+
 echo "** Run mysqld in safe mode **"
 /usr/bin/mysqld_safe --user=mysql --datadir="/var/lib/mysql"
