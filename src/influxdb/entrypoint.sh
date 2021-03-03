1b69aa41508fb473bda21e7b7b82be8599e0b7ed
AmmoMax <steelratmax@gmail.com>
2021-03-03 12:38:40 +0300

    added mounting local volumes for InfluxDB

--- src/influxdb/entrypoint.sh
+++ src/influxdb/entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Description: Preparing influxdb and run setup
 
-echo "** Download influxdb distr **"
-wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.4-linux-amd64.tar.gz
+# echo "** Download influxdb distr **"
+# wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.4-linux-amd64.tar.gz
 
 echo "** Unpack tar archive with influxdb **"
 tar xvfz influxdb2-2.0.4-linux-amd64.tar.gz
