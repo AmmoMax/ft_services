1842ab930eeeebd612adf07a01dfd0a1c793b0af
AmmoMax <steelratmax@gmail.com>
2021-03-08 21:01:53 +0300

    added supervisor for nginx service deployment

--- src/nginx/entrypoint.sh
+++ src/nginx/entrypoint.sh
@@ -1,12 +1,13 @@
 # !/bin/sh
+
 echo "** Add user for ssh access**"
 adduser -D test
+
 echo "** Set the password for user **"
 echo test:123 | chpasswd
+
 echo "** Creating an SSH key **"
 ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-echo "** Run SSHD **"
-/usr/sbin/sshd
-echo "** Starting nginx daemon **"
-nginx -g "daemon off;"
 
+echo "** Run supervisord with custom config **"
+/usr/bin/supervisord -c /etc/supervisord.conf
