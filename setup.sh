63ef8e419e42bb152c2aeab035962c46e6949685
AmmoMax <steelratmax@gmail.com>
2021-03-09 00:32:59 +0300

    rename src dir to srcs

--- setup.sh
+++ setup.sh
@@ -24,61 +24,61 @@ echo "** Configure environment to use minikube’s Docker daemon **"
 eval $(minikube docker-env)
 
 echo "** Deploy configmap for metallb **"
-kubectl apply -f src/metallb/metallb-configmap.yaml
+kubectl apply -f srcs/metallb/metallb-configmap.yaml
 
 ############################################################
 # Now let's build all the docker images we need. See below #
 ############################################################
 
 echo "** Build docker image for nginx **"
-docker build -t amayor_nginx_image src/nginx
+docker build -t amayor_nginx_image srcs/nginx
 
 echo "** Build image for mysql **"
-docker build -t amayor_mysql_image src/mysql
+docker build -t amayor_mysql_image srcs/mysql
 
 echo "** Build image for wordpress **"
-docker build -t amayor_wordpress_image src/wordpress
+docker build -t amayor_wordpress_image srcs/wordpress
 
 echo "** Build image for phpmyadmin **"
-docker build -t amayor_pma_image src/phpmyadmin
+docker build -t amayor_pma_image srcs/phpmyadmin
 
 echo "** Build image for ftps server **"
-docker build -t amayor_ftps_image src/ftps
+docker build -t amayor_ftps_image srcs/ftps
 
 echo "** Build image for grafana server **"
-docker build -t amayor_grafana_image src/grafana
+docker build -t amayor_grafana_image srcs/grafana
 
 echo "** Build image for influxdb server **"
-docker build -t amayor_influxdb_image src/influxdb
+docker build -t amayor_influxdb_image srcs/influxdb
 
 ############################################################
 # Start deploy for any logical services of the our project #
 ############################################################
 
 echo "** Run deployment for nginx **"
-kubectl apply -f src/nginx/nginx.yaml
+kubectl apply -f srcs/nginx/nginx.yaml
 
 echo "** Run deployment for mysql **"
-kubectl apply -f src/mysql/mysql-secret.yaml
-kubectl apply -f src/mysql/mysql-pv.yaml
-kubectl apply -f src/mysql/mysql-pvc.yaml
-kubectl apply -f src/mysql/mysql.yaml
+kubectl apply -f srcs/mysql/mysql-secret.yaml
+kubectl apply -f srcs/mysql/mysql-pv.yaml
+kubectl apply -f srcs/mysql/mysql-pvc.yaml
+kubectl apply -f srcs/mysql/mysql.yaml
 
 echo "** Run deployment for wordpress **"
-kubectl apply -f src/wordpress/wordpress.yaml
+kubectl apply -f srcs/wordpress/wordpress.yaml
 
 echo "** Run deployment for phpmyadmin **"
-kubectl apply -f src/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 
 echo "** Run deployment for ftps server **"
-kubectl apply -f src/ftps/ftps.yaml
+kubectl apply -f srcs/ftps/ftps.yaml
 
 echo "** Run deployment for the influxdb server **"
-kubectl apply -f src/influxdb/influxdb-secret.yaml
-kubectl apply -f src/influxdb/influxdb-pv.yaml
-kubectl apply -f src/influxdb/influxdb-pvc.yaml
-kubectl apply -f src/influxdb/influxdb.yaml
+kubectl apply -f srcs/influxdb/influxdb-secret.yaml
+kubectl apply -f srcs/influxdb/influxdb-pv.yaml
+kubectl apply -f srcs/influxdb/influxdb-pvc.yaml
+kubectl apply -f srcs/influxdb/influxdb.yaml
 
 echo "** Run deployment for the grafana server **"
-kubectl apply -f src/grafana/grafana-secret.yaml
-kubectl apply -f src/grafana/grafana.yaml
+kubectl apply -f srcs/grafana/grafana-secret.yaml
+kubectl apply -f srcs/grafana/grafana.yaml
